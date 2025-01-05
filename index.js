9ab488f76324eb4fb3399b45c96e6e021bb813db
Barabashova Aleksandra <77754618083@mail.ru>
2025-01-05 18:54:56 +0300

    backend Authorization and getMe

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { registerValidation } from "./validations/auth.js"
-import { validationResult } from "express-validator"
-import UserModel from "./models/User.js"
+import checkAuth from "./utils/checkAuth.js";
+
+import * as UserController from "./controllers/UserController.js"
 
 mongoose.connect('mongodb+srv://sasha:11111@cluster0.igcum.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('DB Ok'))
@@ -14,57 +13,11 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/login', async (req, res) => {
-  try{
-    const user  = await UserModel.findOne({ email: req.body.email })
+app.post('/auth/login', UserController.login)
 
-    if (!user) {
-      return req.status(400).json({
-        message: 'Пользователь не найден'
-      })
-    }
+app.post('/auth/register', registerValidation, UserController.register)
 
-  } catch(err) {
-
-  }
-})
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array())
-    }
-  
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-  
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash
-    })
-  
-    const user = await doc.save()
-
-    const token = jwt.sign({
-      _id: user._id
-    }, 'secret123', 
-    {
-      expiresIn: '30d'
-    })
-
-    const { passwordHash, ...userData} = user._doc
-  
-    res.json({...userData, token})
-  } catch(err) {
-    console.log(err)
-    res.status(500).json({
-      message: 'Не удалось зарегистрироваться'
-    })
-}})
+app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.listen(4444, (err) => {
   if (err) {
