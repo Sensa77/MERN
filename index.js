73a6d77803dbccb4a5ecaa36b66be4216a5e2c11
Barabashova Aleksandra <77754618083@mail.ru>
2025-01-05 23:48:48 +0300

    upload image

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 import express from "express";
+import multer from "multer";
 import mongoose from "mongoose";
 import { registerValidation, loginValidation, postCreateValidation } from "./validations.js"
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js"
 import * as PostController from "./controllers/PostControlles.js"
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 mongoose.connect('mongodb+srv://sasha:11111@cluster0.igcum.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('DB Ok'))
 .catch((err) => console.log('DB error', err))
 
 const app = express()
+app.use('/uploads', express.static('uploads'))
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage })
 
 app.use(express.json())
 
-app.post('/auth/login', loginValidation, UserController.login)
-app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  })
+})
 
 app.listen(4444, (err) => {
   if (err) {
